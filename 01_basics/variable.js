4dc11f10967a75954695da4de634a55b0b489115
Beriwala-Safwan <beriwalasafwan345@gmail.com>
2024-09-12 13:20:20 +0000

    |variable in js

---
+++ 01_basics/variable.js
@@ -0,0 +1,8 @@
+const accountId=12345;
+let accountName="safwan";
+var accountType="saving";
+accountCity="ahmedabad";
+
+//accountId=2344; cannot change const value
+
+console.table([accountId,accountName,accountType,accountCity])

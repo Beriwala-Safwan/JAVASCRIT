5f18d09edac3545d0710fb0cf6ecacf2b07a4956
Beriwala-Safwan <beriwalasafwan345@gmail.com>
2024-09-18 12:52:32 +0000

    data type

---
+++ 02_dataType/test.js
@@ -0,0 +1,19 @@
+//DATA TYPES
+let namee="safwan"
+let age =23
+let isAdult=true;
+let maritalStatus;
+let degreeComplete=null;
+
+console.table([namee,age,isAdult,maritalStatus,degreeComplete])
+
+//STRING =""
+//INT =23
+//BOOLEAN =TRUE/FALSE
+//UNDEFINED;
+//NULL=STAND ALONE VALUE
+//SYMBOL=uniqueness
+//OBJECT
+
+console.log(typeof namee);
+console.log(typeof null)

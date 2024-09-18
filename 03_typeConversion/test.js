dbac324a02c8414d8efc9ac9ef873d74b86a6757
Beriwala-Safwan <beriwalasafwan345@gmail.com>
2024-09-18 13:58:57 +0000

    conversion of data type

---
+++ 03_typeConversion/test.js
@@ -0,0 +1,26 @@
+let score=33
+console.log(typeof (score))
+score="33"
+let NmbrConvert=Number(score)
+console.log(typeof NmbrConvert)
+score;
+NmbrConvert=Number(score)
+console.log(NmbrConvert)
+console.log(typeof NmbrConvert)
+
+let marks=0
+let convert=Boolean(marks)
+console.log("marks convert",convert)
+console.log(typeof convert)
+
+//"33" -> 33
+//"33abc"->NaN
+//true =1/false=0
+//" "=false
+//"sfawan" =true
+
+let mark=22
+
+let StringConvert=String(mark)
+console.log("number convert in string",StringConvert)
+console.log(typeof StringConvert)
